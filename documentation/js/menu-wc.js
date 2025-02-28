d67902ecb4df7da8356aab12db5eaf35326a2017
antesbla <antonioestradablanco@gmail.com>
2025-02-28 17:16:49 +0100

    compodoc

--- documentation/js/menu-wc.js
+++ documentation/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">My app documentation</a>
+                    <a href="index.html" data-type="index-link">tel-ahorro-ns documentation</a>
                 </li>
 
                 <li class="divider"></li>
